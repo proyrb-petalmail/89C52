7246dbb588edec2733b767e1cbd36d79aa14f954
ProYRB <143431015+proyrb-petalmail@users.noreply.github.com>
2023-12-28 20:02:53 +0800

    Add files via upload

--- LEDMatrix/LEDMatrix.c
+++ LEDMatrix/LEDMatrix.c
@@ -24,8 +24,8 @@ void LEDMatrix_Light(void) {
 	static unsigned char column = 0, times = 0;
 	
 	for(column = 0; column < 8; column++) {
-		__Column__ = ~(0x80 >> column);
-
+		__Column__ = 0xFF;
+		
 		for(times = 0; times < 8; times++) {
 			if((*(_values_ + ((_index_ + column) % _length_)) & (0x80 >> times)) == (0x80 >> times)) {
 				_DI_ = 1;
@@ -40,6 +40,8 @@ void LEDMatrix_Light(void) {
 		
 		_DO_ = 0;
 		_DO_ = 1;
+		
+		__Column__ = ~(0x80 >> column);
 		
 		LEDMatrix_Delay2ms();
 	}
