c77357c82af90af8e87cda6d950c2115e30805f2
ProYRB <143431015+proyrb-petalmail@users.noreply.github.com>
2024-01-02 09:32:12 +0800

    Delete Example directory

--- Example/LEDQueue/LEDQueue.c
+++
@@ -1,7 +0,0 @@
-#include "LEDQueue.h"
-
-sfr __led_queue__ = 0xA0;
-
-void LEDQueue_Light(const unsigned char number) {
-   __led_queue__ = ~number;
-}
--- Example/LEDQueue/LEDQueue.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LEDQUEUE_H__
-#define __LEDQUEUE_H__
-
-#include <REGX52.H>
-
-void LEDQueue_Light(const unsigned char number); //按照指定的方式点亮
-
-#endif
